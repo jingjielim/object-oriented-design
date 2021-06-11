cbd1b8925576a3056c02ebebbace92de17d1a1d4
jingjielim <jingjie3000@gmail.com>
2021-06-11 13:29:52 +0800

    start on Deck

--- DeckOfCards/src/main/Deck.java
+++ DeckOfCards/src/main/Deck.java
@@ -1,16 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck <T extends Card> {
   private ArrayList<T> cards; // all cards, dealt or not
   private int dealtIndex = 0; // marks index of first undealt card
 
-  public Deck(){
-
+  public Deck(ArrayList<T> deckOfCards){
+    this.cards = deckOfCards;
   }
 
   public void shuffle(){
-
+    for (int i = 0; i < cards.size(); i++) {
+      Random random = new Random();
+    }
   }
 }
